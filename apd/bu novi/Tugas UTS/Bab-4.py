a8de79fbbe4a233f095787f08c2816fe46c6b760
Mahdi <msar.abdullah.idn@gmail.com>
2025-10-08 23:36:43 +0800

    revisi bab 4

--- apd/bu novi/Tugas UTS/Bab-4.py
+++ apd/bu novi/Tugas UTS/Bab-4.py
@@ -6,7 +6,7 @@ kata_batman = input("R : Kelemahanmu apa, Boss = ").lower()
 while kata_batman != "kelemahan":
     print("\nR : Bohong, kamu harus memberitahu kelemahanmu, Boss.")
     print("(Riddler menyuntikkan gas bius kembali, penghitung dimulai kembali dari angka 10) \n")
-    kata_batman = input("R : Kelemahanmu apa, Boss = ")
+    kata_batman = input("R : Kelemahanmu apa, Boss = ").lower()
 
 print("\n(Penghitung mundur menjadi 0 dan berhenti total) \n")
 print("R : Perulangannya berhenti(?) Kau menemukan syarat True yang membuat perulangan itu mencapai titik False.")
