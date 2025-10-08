1ae6dfbd25991b2847cfeeb60e24e48ff38d97d6
Mahdi <msar.abdullah.idn@gmail.com>
2025-10-08 20:12:41 +0800

    bab 5 nih

---
+++ apd/bu novi/Tugas UTS/Bab-5.py
@@ -0,0 +1,8 @@
+kelemahan_batman = {"ketakutan_kehilangan", "ketergantungan_alfred", "rasa_bersalah_orang_tua"}
+print("Kelemahan Batman =", kelemahan_batman)
+
+simbol_burung = "lokasi_batcave_rahasia"
+
+print("\n(Riddler menambahkan kelemahan batman lagi)")
+kelemahan_batman.add(simbol_burung)
+print("Kelemahan Batman = ", kelemahan_batman)
