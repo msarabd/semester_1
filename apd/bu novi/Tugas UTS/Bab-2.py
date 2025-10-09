a949b1d424d5224c622cd04bc961898ff43a8ddc
Mahdi <msar.abdullah.idn@gmail.com>
2025-10-09 15:35:43 +0800

    perubahan var i jadi var a

--- apd/bu novi/Tugas UTS/Bab-2.py
+++ apd/bu novi/Tugas UTS/Bab-2.py
@@ -6,14 +6,14 @@ if angka % 2 == 1:
     warna = "BIRU"
     print(f"R : {angka} adalah bilangan ganjil, tekan tombol {warna}.")
     print(f"(Batman menekan tombol {warna})")
-    print(f"(Laser mati, pintu baja kiri terbuka) \n")
+    print("(Laser mati, pintu baja kiri terbuka) \n")
     list_barang = ["Lampu", "Kotak masuk", "Obor emas", "Peta kuno", "Gembok tua"]
     print("List barang =", list_barang)
 
     barang_ambil = set()
-    for i in list_barang:
-        if i.lower().count("o") >= 1 and i.lower().count("m") >= 1:
-            barang_ambil.add(i)
+    for a in list_barang:
+        if a.lower().count("o") >= 1 and a.lower().count("m") >= 1:
+            barang_ambil.add(a)
 
     print(f"\n(Batman mengambil = {barang_ambil})")
     print("(Lantai bergetar dan sebuah lubang tersembunyi terbuka)")
@@ -22,5 +22,5 @@ else:
     warna = "MERAH"
     print(f"R : {angka} bukan bilangan ganjil, tekan tombol {warna}.")
     print(f"(Batman menekan tombol {warna})")
-    print(f"(Laser mati, pintu baja kanan terbuka) \n")
+    print("(Laser mati, pintu baja kanan terbuka) \n")
     print("(Batman menemukan Riddler)")
--- apd/bu novi/Tugas UTS/Bab-3.py
+++ apd/bu novi/Tugas UTS/Bab-3.py
@@ -1,12 +1,12 @@
 tuple_angka = ((1, 5, 9), (2, 4, 8), (10, 20, 30), (4, 4, 4), (1, 2, 3))
 print("Tuple angka =", tuple_angka)
 
-for i in tuple_angka:
-    jumlah = sum(i)
+for a in tuple_angka:
+    jumlah = sum(a)
     if jumlah % 3 != 0:
-        print(f"jumlah = {jumlah} >> pada tuple ke {i} \n")
-        print(f"R : Tuple {i} tidak habis dibagi 3. \n")
-        kunci_tuple = i
+        print(f"jumlah = {jumlah} >> pada tuple ke {a} \n")
+        print(f"R : Tuple {a} tidak habis dibagi 3. \n")
+        kunci_tuple = a
         print(f"B : Maka, kode mesin = {kunci_tuple}.")
         break
 
